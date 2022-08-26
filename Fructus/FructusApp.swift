59caf10a5c777beebd9177e4a8a4fe1ea7b8479b
Elif Beyza <saglame99@gmail.com>
2022-08-26 13:20:05 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by ELİF BEYZA SAĞLAM on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
